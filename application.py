7fb2cd1975030009693aa0a37487bb58d431992f
ASAKHRI <116259161+ASAKHRI@users.noreply.github.com>
2024-05-06 15:02:10 +0200

    application dev works

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import pickle
-from sklearn.preprocessing import RobustScaler, MinMaxScaler
+from sklearn.preprocessing import RobustScaler, MinMaxScaler, LabelEncoder
 
 with open('models/xgb_v3.pkl','rb') as file :
     model = pickle.load(file)
@@ -20,58 +21,34 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.subheader('Type de contrat')
-    type_contrat_options = ['Cash Loans', 'Revolving Loans']
-    selected_contract = st.selectbox('Sélectionnez le pays:', type_contrat_options)
+    NAME_CONTRACT_TYPE = ['Cash Loans', 'Revolving Loans']
+    selected_contract = st.selectbox('Sélectionnez le pays:', NAME_CONTRACT_TYPE)
 
     # Catégorie Flag own car
     st.subheader('Véhiculé')
-    véhiculé_options = ['Oui','Non']
-    selected_Véhiculé = st.selectbox('Etes-vous le propriétaire de votre véhicule', véhiculé_options)
-
-    # Catégorie Accès au téléphone
-    st.subheader('Propriétaire')
-    proprétaire_options = ['Oui','Non']
-    selected_proptiétaire = st.selectbox('Propriétaire', proprétaire_options)
+    FLAG_OWN_CAR = ['Oui','Non']
+    selected_Véhicule = st.selectbox('Etes-vous le propriétaire de votre véhicule', FLAG_OWN_CAR)
 
 ########## numériques
- # Sidebar pour la taille du ménage
-    st.subheader('cotation_1')
-    selected_cotation_1 = st.slider('cotation_1:', min_value=0.0, max_value=1.0, step = 0.001)
-    
-    st.subheader('cotation_2')
-    selected_cotation_2 = st.slider('cotation_2:', min_value=0.0, max_value=1.0, step = 0.001)
-
-     # Sidebar pour la taille du ménage
-    st.subheader('cotation_3')
-    selected_cotation_3 = st.slider('cotation_3', min_value=0.0, max_value=1.0, step = 0.001)
+    st.subheader("nombre d'enfants")
+    # Utilisation du widget st.number_input()
+    CNT_CHILDREN = st.number_input("Nombre d'enfants", min_value=0, max_value=10,  step=1)
+    # Affichage de la valeur saisie
+    st.write("Ancienneté professionnelle", CNT_CHILDREN)
     
 with col2:    
-    st.subheader('Age')
+
+    st.subheader('Revenus total')
     # Utilisation du widget st.number_input()
-    selected_age = st.number_input("Age client", min_value=18, max_value=90,  step=1)
+    AMT_INCOME_TOTAL = st.number_input("Age client", min_value=0, max_value=1000000000)
     # Affichage de la valeur saisie
-    st.write("Age", selected_age)
+    st.write("Revenu total", AMT_INCOME_TOTAL)
 
-
-    st.subheader('Ancienneté proessionnelle')
+    st.subheader('Montant Credit')
     # Utilisation du widget st.number_input()
-    selected_ancienneté_professionnelle = st.number_input("Anciennet&é professionnelle", min_value=0, max_value=50,  step=1)
+    AMT_CREDIT_x = st.number_input("Montant du crédit", min_value=1000, max_value=1000000000)
     # Affichage de la valeur saisie
-    st.write("Ancienneté professionnelle", selected_ancienneté_professionnelle)
-    
-    st.subheader('Montant bien voulu')
-    # Utilisation du widget st.number_input()
-    montant_bien = st.number_input("Saisissez une valeur numérique", min_value=1000.0, max_value=100000000.0, step=500.0)
-    # Affichage de la valeur saisie
-    st.write("Montant du bien voulut", montant_bien)
-
-
-    st.subheader('Montant_credit')
-    # Utilisation du widget st.number_input()
-    montant_credit_demandé = st.number_input("Saisissez une valeur numérique", min_value=0.0, max_value=100000000.0, value=1000.0, step=1.0)
-    # Affichage de la valeur saisie
-    st.write("Montant du bien voulut", montant_credit_demandé)
-
+    st.write("Montant du crédit", AMT_CREDIT_x)
 
     st.subheader('periode de credit')
     # periode de credit
@@ -81,50 +58,100 @@ with col2:
 
     st.subheader('annuité')
     # Calcul du montant de l'annuité
-    montant_annuité = montant_credit_demandé / periode_credit
+    AMT_ANNUITY_x = AMT_CREDIT_x / periode_credit
     # Affichage du montant de l'annuité
-    st.write("Montant de l'annuité", montant_annuité)
+    st.write("Montant de l'annuité", AMT_ANNUITY_x)
+
+    st.subheader('Montant bien voulu')
+    # Utilisation du widget st.number_input()
+    AMT_GOODS_PRICE = st.number_input("Saisissez une valeur numérique", min_value=1000.0, max_value=100000000.0, step=500.0)
+    # Affichage de la valeur saisie
+    st.write("Montant du bien voulut", AMT_GOODS_PRICE)
+
+    st.subheader('Age')
+    # Utilisation du widget st.number_input()
+    DAYS_BIRTH = st.number_input("Age client", min_value=18, max_value=90,  step=1)
+    # Affichage de la valeur saisie
+    st.write("Age", DAYS_BIRTH)
 
 
+    st.subheader('Ancienneté proessionnelle')
+    # Utilisation du widget st.number_input()
+    DAYS_EMPLOYED = st.number_input("Anciennet&é professionnelle", min_value=0, max_value=50,  step=1)
+    # Affichage de la valeur saisie
+    st.write("Ancienneté professionnelle", DAYS_EMPLOYED)
 
+    st.subheader("Famille")
+    # Utilisation du widget st.number_input()
+    CNT_FAM_MEMBERS = st.number_input("Membre foyer", min_value=0, max_value=10,  step=1)
+    # Affichage de la valeur saisie
+    st.write("Nombre de personnes dans le foyer", CNT_FAM_MEMBERS)
 
+#####
 
-
-
- # Catégorie name_contract_type
+     # Sidebar pour la taille du ménage
+    st.subheader('cotation_1')
+    EXT_SOURCE_1 = st.slider('cotation_1:', min_value=0.0, max_value=1.0, step = 0.001)
     
+    st.subheader('cotation_2')
+    EXT_SOURCE_2 = st.slider('cotation_2:', min_value=0.0, max_value=1.0, step = 0.001)
 
+     # Sidebar pour la taille du ménage
+    st.subheader('cotation_3')
+    EXT_SOURCE_3 = st.slider('cotation_3', min_value=0.0, max_value=1.0, step = 0.001)
 
-# from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-# import pandas as pd
+################
+################
+label_encoder = LabelEncoder()
 
-# # Données catégorielles
-# type_contrat_options = ['Cash Loans', 'Revolving Loans']
-# selected_contract = st.selectbox('Sélectionnez le pays:', type_contrat_options)
+# Encodage de la variable catégorielle 'selected_contract'
+encoded_contract_type = label_encoder.fit_transform(pd.Series(selected_contract))
 
-# véhiculé_options = ['Oui','Non']
-# selected_Véhiculé = st.selectbox('Etes-vous le propriétaire de votre véhicule', véhiculé_options)
+# Encodage de la variable catégorielle 'selected_Véhicule'
+encoded_flag_own_car = label_encoder.fit_transform(pd.Series(selected_Véhicule))
+# Création de DataFrames pour les variables encodées
+encoded_contract_type_df = pd.DataFrame(encoded_contract_type, columns=['NAME_CONTRACT_TYPE'])
+encoded_flag_own_car_df = pd.DataFrame(encoded_flag_own_car, columns=['FLAG_OWN_CAR'])
 
-# proprétaire_options = ['Oui','Non']
-# selected_proptiétaire = st.selectbox('Propriétaire', proprétaire_options)
+# Création du RobustScaler
+robust_scaler = RobustScaler()
 
-# # Encodage des données catégorielles
-# label_encoder = LabelEncoder()
+# Sélection des variables numériques à normaliser
+numerical_features = pd.DataFrame({
+    'CNT_CHILDREN': [CNT_CHILDREN],
+    'AMT_INCOME_TOTAL': [AMT_INCOME_TOTAL],
+    'AMT_CREDIT_x': [AMT_CREDIT_x],
+    'AMT_ANNUITY_x': [AMT_ANNUITY_x],
+    'AMT_GOODS_PRICE': [AMT_GOODS_PRICE],
+    'DAYS_BIRTH': [DAYS_BIRTH],
+    'DAYS_EMPLOYED': [DAYS_EMPLOYED],
+    'CNT_FAM_MEMBERS': [CNT_FAM_MEMBERS]
+})
 
-# # Pour le codage d'étiquettes
-# selected_contract_encoded = label_encoder.fit_transform(selected_contract)
+# Normalisation des variables numériques avec RobustScaler
+scaled_numerical_features = robust_scaler.fit_transform(numerical_features)
+scaled_numerical_features = pd.DataFrame(scaled_numerical_features, columns=numerical_features.columns)
 
-# # Pour le codage one-hot
-# onehot_encoder = OneHotEncoder(sparse=False)
-# selected_Véhiculé_encoded = label_encoder.fit_transform(selected_Véhiculé)
-# selected_proptiétaire_encoded = label_encoder.fit_transform(selected_proptiétaire)
+# Création du MinMaxScaler
+minmax_scaler = MinMaxScaler()
 
-# # Création d'un DataFrame pandas avec les données encodées
-# data_encoded = pd.DataFrame({
-#     'Type de contrat': selected_contract_encoded,
-#     'Véhiculé': selected_Véhiculé_encoded,
-#     'Propriétaire': selected_proptiétaire_encoded
-# })
+# Sélection des variables à normaliser
+ext_sources = pd.DataFrame({
+    'EXT_SOURCE_1': [EXT_SOURCE_1],
+    'EXT_SOURCE_2': [EXT_SOURCE_2],
+    'EXT_SOURCE_3': [EXT_SOURCE_3]
+})
 
-# # Affichage des données encodées
-# st.write("Données catégorielles encodées :", data_encoded)
+# Normalisation avec MinMaxScaler
+scaled_ext_sources = minmax_scaler.fit_transform(ext_sources)
+scaled_ext_sources = pd.DataFrame(scaled_ext_sources, columns=ext_sources.columns)
+
+# Ajouter un bouton de prédiction
+if st.button('Prédire'):
+    # Concaténer les données encodées et normalisées
+    features = pd.concat([encoded_contract_type_df, encoded_flag_own_car_df, scaled_numerical_features, scaled_ext_sources], axis=1)
+    # Faire la prédiction avec le modèle
+    prediction = model.predict_proba(features)
+
+    # Afficher le résultat de la prédiction
+    st.write("La prédiction de solvabilité du client est :", prediction)
