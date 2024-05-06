4a3f5b517e2923d7973d7bb560bb677819b61c3e
ASAKHRI <116259161+ASAKHRI@users.noreply.github.com>
2024-05-06 14:00:20 +0200

    des application

--- application.py
+++ application.py
@@ -1,13 +1,24 @@
 import streamlit as st
 import pandas as pd
 import pickle
-from sklearn.preprocessing import RobustScaler    
+from sklearn.preprocessing import RobustScaler, MinMaxScaler
+
+with open('models/xgb_v3.pkl','rb') as file :
+    model = pickle.load(file)
 
 
+st.set_page_config(
+    page_title = " :bank: Prédiction de solvabilité client",
+    page_icon= ":bank:",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+st.title(":classical_building: Prediction de solvabilité")
 
 
+col1, col2 = st.columns(2)
 
- # Catégorie name_contract_type
+with col1:
     st.subheader('Type de contrat')
     type_contrat_options = ['Cash Loans', 'Revolving Loans']
     selected_contract = st.selectbox('Sélectionnez le pays:', type_contrat_options)
@@ -34,16 +45,23 @@ from sklearn.preprocessing import RobustScaler
     st.subheader('cotation_3')
     selected_cotation_3 = st.slider('cotation_3', min_value=0.0, max_value=1.0, step = 0.001)
     
-         # Sidebar pour l'âge
-    st.subheader('Âge')
-    selected_age = st.slider('Age_client', min_value=18, max_value=100)
-
-         # Sidebar pour la taille du ménage
-    st.subheader('Ancienneté proessionnelle')
-    selected_household_size = st.slider('Actif depuis', min_value=0, max_value=60, )
-    
+with col2:    
+    st.subheader('Age')
     # Utilisation du widget st.number_input()
-    montant_bien = st.number_input("Saisissez une valeur numérique", min_value=0.0, max_value=100000000.0, value=50.0, step=1.0)
+    selected_age = st.number_input("Age client", min_value=18, max_value=90,  step=1)
+    # Affichage de la valeur saisie
+    st.write("Age", selected_age)
+
+
+    st.subheader('Ancienneté proessionnelle')
+    # Utilisation du widget st.number_input()
+    selected_ancienneté_professionnelle = st.number_input("Anciennet&é professionnelle", min_value=0, max_value=50,  step=1)
+    # Affichage de la valeur saisie
+    st.write("Ancienneté professionnelle", selected_ancienneté_professionnelle)
+    
+    st.subheader('Montant bien voulu')
+    # Utilisation du widget st.number_input()
+    montant_bien = st.number_input("Saisissez une valeur numérique", min_value=1000.0, max_value=100000000.0, step=500.0)
     # Affichage de la valeur saisie
     st.write("Montant du bien voulut", montant_bien)
 
@@ -70,36 +88,43 @@ from sklearn.preprocessing import RobustScaler
 
 
 
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-import pandas as pd
 
-# Données catégorielles
-type_contrat_options = ['Cash Loans', 'Revolving Loans']
-selected_contract = st.selectbox('Sélectionnez le pays:', type_contrat_options)
 
-véhiculé_options = ['Oui','Non']
-selected_Véhiculé = st.selectbox('Etes-vous le propriétaire de votre véhicule', véhiculé_options)
 
-proprétaire_options = ['Oui','Non']
-selected_proptiétaire = st.selectbox('Propriétaire', proprétaire_options)
+ # Catégorie name_contract_type
+    
 
-# Encodage des données catégorielles
-label_encoder = LabelEncoder()
 
-# Pour le codage d'étiquettes
-selected_contract_encoded = label_encoder.fit_transform(selected_contract)
+# from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# import pandas as pd
 
-# Pour le codage one-hot
-onehot_encoder = OneHotEncoder(sparse=False)
-selected_Véhiculé_encoded = label_encoder.fit_transform(selected_Véhiculé)
-selected_proptiétaire_encoded = label_encoder.fit_transform(selected_proptiétaire)
+# # Données catégorielles
+# type_contrat_options = ['Cash Loans', 'Revolving Loans']
+# selected_contract = st.selectbox('Sélectionnez le pays:', type_contrat_options)
 
-# Création d'un DataFrame pandas avec les données encodées
-data_encoded = pd.DataFrame({
-    'Type de contrat': selected_contract_encoded,
-    'Véhiculé': selected_Véhiculé_encoded,
-    'Propriétaire': selected_proptiétaire_encoded
-})
+# véhiculé_options = ['Oui','Non']
+# selected_Véhiculé = st.selectbox('Etes-vous le propriétaire de votre véhicule', véhiculé_options)
 
-# Affichage des données encodées
-st.write("Données catégorielles encodées :", data_encoded)
+# proprétaire_options = ['Oui','Non']
+# selected_proptiétaire = st.selectbox('Propriétaire', proprétaire_options)
+
+# # Encodage des données catégorielles
+# label_encoder = LabelEncoder()
+
+# # Pour le codage d'étiquettes
+# selected_contract_encoded = label_encoder.fit_transform(selected_contract)
+
+# # Pour le codage one-hot
+# onehot_encoder = OneHotEncoder(sparse=False)
+# selected_Véhiculé_encoded = label_encoder.fit_transform(selected_Véhiculé)
+# selected_proptiétaire_encoded = label_encoder.fit_transform(selected_proptiétaire)
+
+# # Création d'un DataFrame pandas avec les données encodées
+# data_encoded = pd.DataFrame({
+#     'Type de contrat': selected_contract_encoded,
+#     'Véhiculé': selected_Véhiculé_encoded,
+#     'Propriétaire': selected_proptiétaire_encoded
+# })
+
+# # Affichage des données encodées
+# st.write("Données catégorielles encodées :", data_encoded)
